37af98ec42b7fe81ddf93e6ebb7150210b379de0
廖 <lsp@nongline.com>
2020-02-21 17:25:33 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,7 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+var utils = require('../../utils/util.js');
 Page({
   data: {
     motto: 'Hello World',
@@ -11,13 +11,17 @@ Page({
     imageList:[
       '/static/image/ad/ad1.jpg',
       '/static/image/ad/ad2.jpg',
-      '/static/image/ad/ad3.jpg',
+    ],
+    navList: [
+      { icon: '/static/nav-icon/diantai.png', events: 'goToBangDan', text: '榜单' },
+      { icon: '/static/nav-icon/diantai.png', events: 'goToBangDan', text: '听小说' },
+      { icon: '/static/nav-icon/diantai.png', events: 'goToBangDan', text: '情感电台' },
+      { icon: '/static/nav-icon/diantai.png', events: 'goToBangDan', text: '听知识' },
     ],
     swiperCurrent: 0
   },
   //轮播图改变事件
   swiperChange: function (e) {
-    //console.log(e.detail.current);
     this.setData({
       swiperCurrent: e.detail.current
     })
@@ -26,7 +30,25 @@ Page({
     
   },
   onLoad: function () {
-    
+    var that = this;
+    var url = "http://mobile.ximalaya.com/mobile/discovery/v3/recommend/hotAndGuess?code=43_310000_3100&device=android&version=5.4.45";
+    utils.myRequest({
+      url:url,
+      method:'GET',
+      success:function(result){
+        that.setData({
+          showitem: true,
+          guess: result.data.paidArea.list,
+          xiaoshuocontent: result.data.hotRecommends.list[0].list,
+          xiangshengcontent: result.data.hotRecommends.list[2].list,
+          tuokocontent: result.data.hotRecommends.list[4].list
+        })
+      },
+      fail:function(result){
+
+      }
+    })
+
 
   },
 
--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,31 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
-module.exports = {
-  formatTime: formatTime
+
+let myRequest = function (args = { url: '', methods: 'GET', data: {}, success: function () { }, fail: function () { } }) {
+  wx.request({
+    url: args.url,
+    data: args.data,
+    header: { 'content-type': 'application/json' },
+    method: args.methods,
+    dataType: 'json',
+    responseType: 'text',
+    success: (res) => {
+      console.log(res);
+      if (res.statusCode == 200) {
+        // 请求成功执行回调函数
+        args.success(res)
+      } else {
+        // 请求失败执行回调函数
+        args.fail()
+      }
+    },
+  })
 }
+
+module.exports = {
+  formatTime: formatTime,
+  myRequest: myRequest
+}
+
+
